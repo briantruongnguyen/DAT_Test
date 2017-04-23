a25f8d8cd88088a9d48c78e99e72cc80f69c946d
megazeroxuxm <megazeroxuxm@gmail.com>
2017-04-23 17:10:58 -0500

    Update angles.js

--- public/js/angles.js
+++ public/js/angles.js
@@ -175,18 +175,18 @@ function draw() {
     
     var correctAnswer;
     if(choiceToDegree[choices[0]] < choiceToDegree[choices[1]]){
-        correctAnswer = minAngleAnswer +", "+ choices[0] + ", " + choices[1] + ", " + maxAngleAnswer 
-        answer1 = choices[0] +", "+ minAngleAnswer + ", " + choices[1] + ", " +maxAngleAnswer
-        answer2 = choices[0] +", "+ minAngleAnswer + ", " + maxAngleAnswer  + ", " + choices[1]
-        answer3 = minAngleAnswer +", "+ choices[0] + ", " + choices[1] + ", " + maxAngleAnswer 
-        answer4 = minAngleAnswer +", "+ choices[0] + ", " + maxAngleAnswer + ", " + choices[1] 
+        correctAnswer = minAngleAnswer +"< "+ choices[0] + "< " + choices[1] + "< " + maxAngleAnswer 
+        answer1 = choices[0] +"< "+ minAngleAnswer + "< " + choices[1] + "< " +maxAngleAnswer
+        answer2 = choices[0] +"< "+ minAngleAnswer + "< " + maxAngleAnswer  + "< " + choices[1]
+        answer3 = minAngleAnswer +"< "+ choices[0] + "< " + choices[1] + "< " + maxAngleAnswer 
+        answer4 = minAngleAnswer +"< "+ choices[0] + "< " + maxAngleAnswer + "< " + choices[1] 
     }
     else{
-        correctAnswer = minAngleAnswer +", "+ choices[1] + ", " + choices[0] + ", " + maxAngleAnswer 
-        answer1 = choices[1] +", "+ minAngleAnswer + ", " + choices[0] + ", " +maxAngleAnswer
-        answer2 = choices[1] +", "+ minAngleAnswer + ", " + maxAngleAnswer  + ", " + choices[0]
-        answer3 = minAngleAnswer +", "+ choices[1] + ", " + choices[0] + ", " + maxAngleAnswer 
-        answer4 = minAngleAnswer +", "+ choices[1] + ", " + maxAngleAnswer + ", " + choices[0] 
+        correctAnswer = minAngleAnswer +"< "+ choices[1] + "< " + choices[0] + "< " + maxAngleAnswer 
+        answer1 = choices[1] +"< "+ minAngleAnswer + "< " + choices[0] + "< " +maxAngleAnswer
+        answer2 = choices[1] +"< "+ minAngleAnswer + "< " + maxAngleAnswer  + "< " + choices[0]
+        answer3 = minAngleAnswer +"< "+ choices[1] + "< " + choices[0] + "< " + maxAngleAnswer 
+        answer4 = minAngleAnswer +"< "+ choices[1] + "< " + maxAngleAnswer + "< " + choices[0] 
     }
 
     var answers = [answer1, answer2, answer3, answer4]
@@ -270,93 +270,8 @@ function showAnswer() {
 	}
 }
 
-//highjghjghjghj/////////////////////// TYPING THINGY
 
-var temp = document.querySelector('.time');
-var button = document.querySelector("button");
-var words = document.querySelector(".words");
-var timerDiv = document.querySelector(".time");
-var scoreDiv = document.querySelector(".score");
-var points = 0;
-var spans;
-var typed;
-var seconds = 60;
-
-function countdown() {
-	points = 0;
-	var timer = setInterval(function(){
-			button.disabled = true;
-			seconds--;
-			temp.innerHTML = seconds;
-			if (seconds === 0) {
-			alert("Game over! Your score is " + points);
-			scoreDiv.innerHTML = "0";
-			words.innerHTML = "";
-			button.disabled = false;
-			clearInterval(timer);
-			seconds = 60;
-			timerDiv.innerHTML = "60";
-			button.disabled = false;	
-			}
-			}, 1000);
-}
-
-function random() {
-	words.innerHTML = "";
-	var random = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
-	var wordArray = list[random].split("");
-	for (var i = 0; i < wordArray.length; i++) { //building the words with spans around the letters
-		var span = document.createElement("span");
-		span.classList.add("span");
-		span.innerHTML = wordArray[i];
-		words.appendChild(span);
-	}
-	spans = document.querySelectorAll(".span");
-}
-
-
-const list = ['ABCD','ACDB','BCDA','ADCB']
-
-button.addEventListener("click", function(e){
-		countdown();
-		random();
-		button.disabled = true;	
-		});
-
-
-function typing(e) {
-	typed = String.fromCharCode(e.which);
-	for (var i = 0; i < spans.length; i++) {
-		if	(spans[i].innerHTML === typed) { // if typed letter is the one from the word
-			if (spans[i].classList.contains("bg")) { // if it already has class with the bacground color then check the next one
-				continue;
-			} else if (spans[i].classList.contains("bg") === false && spans[i-1] === undefined || spans[i-1].classList.contains("bg") !== false ) { // if it dont have class, if it is not first letter or if the letter before it dont have class (this is done to avoid marking the letters who are not in order for being checked, for example if you have two "A"s so to avoid marking both of them if the first one is at the index 0 and second at index 5 for example)
-				spans[i].classList.add("bg");
-				break;
-			}
-		}
-	}
-	var checker = 0;
-	for (var j = 0; j < spans.length; j++) { //checking if all the letters are typed
-		if (spans[j].className === "span bg") {
-			checker++;
-		}
-		if (checker === spans.length) { // if so, animate the words with animate.css class
-			spark.currentTime = 0;
-			spark.play();
-			words.classList.add("animated");
-			words.classList.add("fadeOut");
-			points++; // increment the points
-			scoreDiv.innerHTML = points; //add points to the points div
-			document.removeEventListener("keydown", typing, false);
-			setTimeout(function(){
-					words.className = "words"; // restart the classes
-					random(); // give another word
-					document.addEventListener("keydown", typing, false);
-					}, 400);
-		}
 
 	}
 }
 
-document.addEventListener("keydown", typing, false); 
