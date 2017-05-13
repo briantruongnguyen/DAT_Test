4ad0de5768b7585216f3b2f2378197fd039a2ec1
megazeroxuxm <megazeroxuxm@gmail.com>
2017-05-13 15:35:57 -0500

    Update angles.js

--- public/js/angles.js
+++ public/js/angles.js
@@ -210,7 +210,7 @@ function createAnswers(answers, correctanswer) {
       item = $('<li>');
       input = '<input type="radio" name="answer" value=' + i + ' />';
       input += answers[i];
-	input += '<button onclick='checkAnswer()'>Submit Answer</button>';
+	
       item.append(input);
       radioList.append(item);
     }
