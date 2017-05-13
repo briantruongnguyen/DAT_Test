e08bac47bb6c00e44953465ea45dfa76d94d61fb
megazeroxuxm <megazeroxuxm@gmail.com>
2017-05-13 17:38:28 -0500

    Update angles.js

--- public/js/angles.js
+++ public/js/angles.js
@@ -222,7 +222,7 @@ function createAnswers(answers, correctanswer) {
 function checkAnswer() 
 
 	{
-	var selections = document.getElementsByName("answer");
+	var selections = document.getElementsByName("answers");
 	for (var i=0; i < selections.length; i++) 
 		{
 		if(selections[i].checked)
